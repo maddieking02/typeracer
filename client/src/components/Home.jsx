dce9105669d44f8d238ef1ef5b616993f543d5ac
Madeline King <madeline.king330@gmail.com>
2023-01-11 21:46:21 -0800

    add css to home page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -17,17 +17,17 @@ const Home = () => {
   const user = location.state.user === 'guest' ? (location.state.user.charAt(0).toUpperCase() + location.state.user.slice(1)) + JSON.stringify(Math.floor(Math.random() * 100) + 1) : location.state.user;
 
   return (
-    <div className="bg">
-      <div className="star-field">
+    <div className="bg home-container">
+      <div className="star-field home-inner-container">
         <div className="layer" />
         <div className="layer" />
         <h1 id="header">typeracer_</h1>
         <h2>Welcome {user}</h2>
-        <img src={logo} alt="" />
-        <h1>A typeracing game for programmers.</h1>
-        <button type="button" name="/game/create" onClick={(e) => {routeChange(e)}}>CREATE GAME ></button>
-        <button type="button" name="/game/join" onClick={(e) => {routeChange(e)}}>JOIN GAME ></button>
-        <button type="button" name="/race" onClick={(e) => {routeChange(e)}}>START RACING ></button>
+        <img id="home-logo" src={logo} alt="" />
+        <h1 id="slogan">A typeracing game for programmers.</h1>
+        <button className="home-btn glow-on-hover" type="button" name="/game/create" onClick={(e) => {routeChange(e)}}>CREATE GAME ></button>
+        <button className="home-btn glow-on-hover" type="button" name="/game/join" onClick={(e) => {routeChange(e)}}>JOIN GAME ></button>
+        <button className="home-btn glow-on-hover" type="button" name="/race" onClick={(e) => {routeChange(e)}}>START RACING ></button>
       </div>
     </div>
   );
