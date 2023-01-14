25ffd51436c01e7e30fc8e740eb4563d3cc9c78c
Madeline King <madeline.king330@gmail.com>
2023-01-14 01:08:29 -0800

    add account and settings components

---
+++ client/src/components/Account.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+const Account = () => {
+  return (
+    <div>
+      I am account.
+    </div>
+  );
+};
+
+export default Account;
---
+++ client/src/components/Settings.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+const Settings = () => {
+  return (
+    <div>
+      I am Settings.
+    </div>
+  );
+};
+
+export default Settings;
